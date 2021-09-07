ceffe061d371c69693e72f05d5f1648bb4b1c3d1
nadir <alinadir33@gmail.com>
2021-09-07 11:35:38 +0500

    shared preference

--- app/src/main/java/com/ido/nadir/firstapplication/Dashboard.java
+++ app/src/main/java/com/ido/nadir/firstapplication/Dashboard.java
@@ -4,36 +4,48 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 public class Dashboard extends AppCompatActivity {
+    public static final String PREF = "myPref";
     private static final String TAG = "Dashboard";
     TextView tvGreetings;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
         tvGreetings = findViewById(R.id.tvGreetings);
-        String userName = getIntent().getExtras().getString("USER");
-        tvGreetings.setText("Welcome "+userName);
-        Log.e(TAG, "onCreate: First Function" );
+        if (getIntent().getExtras()!= null) {
+            String userName = getIntent().getExtras().getString("USER");
+            tvGreetings.setText("Welcome " + userName);
+        }
+        Log.e(TAG, "onCreate: First Function");
+    }
+
+    public void LogOut(View view) {
+        //TODO call sharedPreference PREF and set its check variable to false
+
+
+        //TODO call activity Main from intent and close this activity with finish();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.e(TAG, "onPause: Second Function" );
+        Log.e(TAG, "onPause: Second Function");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.e(TAG, "onStop: is called" );
+        Log.e(TAG, "onStop: is called");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.e(TAG, "onDestroy: is called" );
+        Log.e(TAG, "onDestroy: is called");
     }
 }
--- app/src/main/java/com/ido/nadir/firstapplication/MainActivity.java
+++ app/src/main/java/com/ido/nadir/firstapplication/MainActivity.java
@@ -3,10 +3,12 @@ package com.ido.nadir.firstapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -15,10 +17,12 @@ import javax.security.auth.login.LoginException;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
+    public static final String PREF = "myPref";
     Button btLogin;
     EditText etUserName;
     EditText etPassword;
     TextView tvRegister;
+    CheckBox cbStaySigned;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
         etUserName = findViewById(R.id.etUserName);
         etPassword = findViewById(R.id.etPassword);
         tvRegister = findViewById(R.id.tvRegister);
+        cbStaySigned = findViewById(R.id.cbStaySigned);
         Log.e(TAG, "onCreate: Last Line");
         tvRegister.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,7 +52,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         Log.e(TAG, "onResume: is called");
+        SharedPreferences sharedPreferences = getSharedPreferences(PREF,MODE_PRIVATE);
+        String name = sharedPreferences.getString("name","");
+        etUserName.setText(name);
+        boolean check = sharedPreferences.getBoolean("check",false);
+        if(check){
+            Intent intent = new Intent(MainActivity.this, Dashboard.class);
+            startActivity(intent);
+        }
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SharedPreferences sharedPreferences = getSharedPreferences(PREF, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("name", etUserName.getText().toString());
+        editor.apply();
+
+    }
+
+
 
     public void LoginClicked(View view) {
 //        try{
@@ -59,9 +84,15 @@ public class MainActivity extends AppCompatActivity {
         String password;
         userName = etUserName.getText().toString();
         password = etPassword.getText().toString();
-        if(userName.equals("test")&&password.equals("123")){
+        if(password.equals("123")){
             //Move to next page
 //            Toast.makeText(MainActivity.this, "Welcome test user", Toast.LENGTH_SHORT).show();
+            if(cbStaySigned.isChecked()){
+                SharedPreferences sharedPreferences = getSharedPreferences(PREF,MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putBoolean("check",true);
+                editor.apply();
+            }
             Intent intent = new Intent(MainActivity.this,Dashboard.class);
             intent.putExtra("USER",userName);
             startActivity(intent);
