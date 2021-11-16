9f349e7b8c934bfa5e60a5aa9f65327f3ad7ae15
nadir <alinadir33@gmail.com>
2021-11-16 12:06:46 +0500

    SQLite Create Read

--- app/src/main/java/com/ido/nadir/firstapplication/Dashboard.java
+++ app/src/main/java/com/ido/nadir/firstapplication/Dashboard.java
@@ -2,22 +2,43 @@ package com.ido.nadir.firstapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Dashboard extends AppCompatActivity {
     public static final String PREF = "myPref";
     private static final String TAG = "Dashboard";
     TextView tvGreetings;
+    EditText etName;
+    EditText etCurrency;
+    DatabaseHelper dbHelper;
+    TextView tvShowValues;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
         tvGreetings = findViewById(R.id.tvGreetings);
-        if (getIntent().getExtras()!= null) {
+        dbHelper = new DatabaseHelper(Dashboard.this);
+
+        etName = findViewById(R.id.etName);
+        etCurrency = findViewById(R.id.etCurrency);
+        tvShowValues = findViewById(R.id.tvShowData);
+        String value = "";
+        Cursor cursor = dbHelper.getCurrencyDetails();
+        if (cursor.moveToFirst()) {
+            do {
+                value += cursor.getString(cursor.getColumnIndex(DatabaseHelper.NAME)) +
+                        cursor.getString(cursor.getColumnIndex(DatabaseHelper.CURRENCY));
+            } while (cursor.moveToNext());
+        }
+        tvShowValues.setText(value);
+        if (getIntent().getExtras() != null) {
             String userName = getIntent().getExtras().getString("USER");
             tvGreetings.setText("Welcome " + userName);
         }
@@ -47,5 +68,16 @@ public class Dashboard extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         Log.e(TAG, "onDestroy: is called");
+    }
+
+    public void InsertData(View view) {
+        if (etName.getText().toString().equals("") || etCurrency.getText().toString().equals("")) {
+            Toast.makeText(Dashboard.this, "Pullo Chezy blik", Toast.LENGTH_SHORT).show();
+        } else {
+            String name = etName.getText().toString();
+            String currency = etCurrency.getText().toString();
+            dbHelper.insertIntoTable(name, currency);
+            Toast.makeText(Dashboard.this, "Data inserted successfully", Toast.LENGTH_SHORT).show();
+        }
     }
 }
---
+++ app/src/main/java/com/ido/nadir/firstapplication/DatabaseHelper.java
@@ -0,0 +1,55 @@
+package com.ido.nadir.firstapplication;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "currencyDB";
+    public static final int DATABASE_VERSION = 1;
+    public static final String TABLE_CURRENCY_DETAIL = "currency_details";
+    public static final String ID = "id";
+    public static final String NAME = "name";
+    public static final String CURRENCY = "currency";
+    public static final String CREATE_TABLE_CURRENCY = "CREATE TABLE "+TABLE_CURRENCY_DETAIL+
+            " ("+ID+" integer primary key AUTOINCREMENT, "+NAME+" varchar(20), "+
+            CURRENCY+" varchar(20))";
+
+
+    public DatabaseHelper(@Nullable Context context, @Nullable String name, int version) {
+        super(context, name, null, version);
+    }
+
+    public DatabaseHelper(Context paramContext) {
+        super(paramContext, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(CREATE_TABLE_CURRENCY);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+
+    public void insertIntoTable(String name, String currency){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(NAME,name);
+        values.put(CURRENCY,currency);
+        db.insert(TABLE_CURRENCY_DETAIL,null,values);
+        db.close();
+    }
+
+    public  Cursor getCurrencyDetails(){
+        String query = "select * from "+TABLE_CURRENCY_DETAIL;
+        SQLiteDatabase db = this.getReadableDatabase();
+        return db.rawQuery(query,null);
+    }
+}
