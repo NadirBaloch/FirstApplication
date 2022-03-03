189710d9319e260aed396f408bc5ccd52798108f
NadirBaloch <alinadir33@gmail.com>
2022-03-03 12:53:11 +0500

    jsonParsing + Asynck

--- app/src/main/java/com/ido/nadir/firstapplication/MainActivity.java
+++ app/src/main/java/com/ido/nadir/firstapplication/MainActivity.java
@@ -1,6 +1,5 @@
 package com.ido.nadir.firstapplication;
 
-import static java.sql.DriverManager.println;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,20 +9,28 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.view.textclassifier.TextLinks;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import javax.security.auth.login.LoginException;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+    String data = "init ";
+    String studentName;
+    String topic;
+    boolean isSubmitted;
+    DatabaseHelper databaseHelper;
+
     public String url = "https://6217195471e7672e53735dca.mockapi.io/api/v1/test/2";
     private static final String TAG = "MainActivity";
     public static final String PREF = "myPref";
@@ -31,50 +38,77 @@ public class MainActivity extends AppCompatActivity {
     EditText etUserName;
     EditText etPassword;
     TextView tvRegister;
+    TextView tvAPIData;
     CheckBox cbStaySigned;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         btLogin = findViewById(R.id.btLogin);
         etUserName = findViewById(R.id.etUserName);
         etPassword = findViewById(R.id.etPassword);
         tvRegister = findViewById(R.id.tvRegister);
         cbStaySigned = findViewById(R.id.cbStaySigned);
+        tvAPIData = findViewById(R.id.tvAPIData);
         Log.e(TAG, "onCreate: Last Line");
 
         OKHTTPHandler okhttpHandler = new OKHTTPHandler();
         okhttpHandler.execute();
+
     }
 
-    public class OKHTTPHandler extends AsyncTask {
+    public class OKHTTPHandler extends AsyncTask<String, String, String> {
+        String myData;
+        JSONObject jObject;
+
         @Override
-        protected void onPostExecute(Object o) {
-            super.onPostExecute(o);
-            Log.i(TAG, "onPostExecute: download finished");
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            try {
+                jObject = new JSONObject(s);
+//                JSONArray jarray  = new JSONArray(s);
+                studentName = jObject.getString("studentname");
+                topic = jObject.getString("topic");
+                isSubmitted = jObject.getBoolean("isSubmitted");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            Log.i(TAG, "onPostExecute: after download" + s);
+            tvAPIData.setText(studentName + " " + topic);
+//            tvAPIData.setText(s);
+            Log.i(TAG, "onPostExecute: " + myData);
+        }
+
+        OKHTTPHandler() {
+            myData = "nothing Yet";
         }
 
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
+            tvAPIData.setText("Downloading");
             Log.i(TAG, "onPreExecute: I am about to download names");
         }
 
-        OkHttpClient client = new OkHttpClient();
         @Override
-        protected Object doInBackground(Object[] objects) {
-                Request.Builder builder = new Request.Builder();
-                builder.url(url);
-                Request request =builder.build();
-                try{
-                    Response response = client.newCall(request).execute();
-                    Log.i(TAG, "download doInBackground: "+response.body().string());
-                    return response.body().string();
-                }catch(Exception e){
-                    e.printStackTrace();
-                }
-            return null;
+        protected String doInBackground(String... strings) {
+            OkHttpClient client = new OkHttpClient();
+            Request.Builder builder = new Request.Builder();
+            builder.url(url);
+            Request request = builder.build();
+            try {
+                Response response = client.newCall(request).execute();
+                //Log.i(TAG, "doInBackground: "+response.body().string());
+                return "" + response.body().string();
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
         }
+
+
     }
 
     @Override
@@ -87,11 +121,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         Log.e(TAG, "onResume: is called");
-        SharedPreferences sharedPreferences = getSharedPreferences(PREF,MODE_PRIVATE);
-        String name = sharedPreferences.getString("name","");
+        SharedPreferences sharedPreferences = getSharedPreferences(PREF, MODE_PRIVATE);
+        String name = sharedPreferences.getString("name", "");
         etUserName.setText(name);
-        boolean check = sharedPreferences.getBoolean("check",false);
-        if(check){
+        boolean check = sharedPreferences.getBoolean("check", false);
+        if (check) {
             Intent intent = new Intent(MainActivity.this, Dashboard.class);
             startActivity(intent);
         }
@@ -108,7 +142,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
     public void LoginClicked(View view) {
 //        try{
 //            Toast.makeText(MainActivity.this,etUserName.getText().toString(), Toast.LENGTH_SHORT).show();
@@ -119,23 +152,23 @@ public class MainActivity extends AppCompatActivity {
         String password;
         userName = etUserName.getText().toString();
         password = etPassword.getText().toString();
-        if(password.equals("123")){
+        if (password.equals("123")) {
             //Move to next page
 //            Toast.makeText(MainActivity.this, "Welcome test user", Toast.LENGTH_SHORT).show();
-            if(cbStaySigned.isChecked()){
-                SharedPreferences sharedPreferences = getSharedPreferences(PREF,MODE_PRIVATE);
+            if (cbStaySigned.isChecked()) {
+                SharedPreferences sharedPreferences = getSharedPreferences(PREF, MODE_PRIVATE);
                 SharedPreferences.Editor editor = sharedPreferences.edit();
-                editor.putBoolean("check",true);
+                editor.putBoolean("check", true);
                 editor.apply();
             }
-            Intent intent = new Intent(MainActivity.this,Dashboard.class);
-            intent.putExtra("USER",userName);
+            Intent intent = new Intent(MainActivity.this, Dashboard.class);
+            intent.putExtra("USER", userName);
             startActivity(intent);
-        }else{
+        } else {
             Toast.makeText(MainActivity.this, "Username or password incorrect", Toast.LENGTH_SHORT).show();
-            Log.e(TAG, "LoginClicked: " );
+            Log.e(TAG, "LoginClicked: ");
         }
-        Log.e(TAG, "LoginClicked: "+userName);
+        Log.e(TAG, "LoginClicked: " + userName);
     }
 
     public void registerButtonClicked(View view) {
